0629680f6c22bf99aed689412e55b1bc73c43f59
IlhamNR <ilhamnurramadhan19@gmail.com>
2023-09-06 14:20:32 +0700

    landing page update

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -29,13 +29,13 @@
         </div>
     </header>
     <div
-        class=" mx-8 my-8 text-center rounded-lg bg-white p-8 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        class=" mx-8 my-8 text-center rounded-lg bg-white md:p-8 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <h1 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
             Our Products
         </h1>
-        <div class="flex flex-row justify-center">
+        <div class="flex flex-row max-md:flex-col justify-center">
             <div
-                class="mx-8 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                class="mx-8 max-md:my-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                 <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init
                     data-te-ripple-color="light">
                     <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
@@ -54,7 +54,7 @@
                 </div>
             </div>
             <div
-                class="mx-8 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                class="mx-8 max-md:my-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                 <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init
                     data-te-ripple-color="light">
                     <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
@@ -73,7 +73,7 @@
                 </div>
             </div>
             <div
-                class="mx-8 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                class="mx-8 max-md:my-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                 <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init
                     data-te-ripple-color="light">
                     <img class="rounded-t-lg" src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
