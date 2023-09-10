8c39622c25947e4afa184301f08abfc3b9051ab9
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-09-10 13:41:36 +0700

    restore tailwindcss props

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,26 @@
-import './bootstrap';
+import "./bootstrap";
+import {
+    Collapse,
+    Ripple,
+    initTE,
+    Dropdown,
+    Input,
+    Modal,
+    Carousel,
+} from "tw-elements";
 
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
 window.Alpine = Alpine;
+
+initTE({
+    Collapse,
+    Ripple,
+    Dropdown,
+    Input,
+    Modal,
+    Carousel,
+});
 
 Alpine.plugin(focus);
 
