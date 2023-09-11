fa9653424138f7c49f25de26862fd521a90a0885
IlhamNR <ilhamnurramadhan19@gmail.com>
2023-09-11 15:31:51 +0700

    update admin page

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.dashboard');
     })->name('dashboard');
 });
