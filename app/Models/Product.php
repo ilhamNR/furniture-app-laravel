384034def2c51627ac8996a05bf585c1dc90cbf4
IlhamNR <ilhamnurramadhan19@gmail.com>
2023-10-16 15:41:01 +0700

    update several models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProductImage;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'name',
+        'product_category_id',
+        'price',
+        'is_available',
+        'description'
+    ];
     use HasFactory;
+
+    public function images()
+    {
+        $this->hasMany(ProductImage::class);
+    }
 }
